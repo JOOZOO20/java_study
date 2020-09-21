4bd0c83fb250558072643dc52e3febc53490778c
JOOZOO20 <qwsa7896@naver.com>
2020-09-21 22:53:33 +0900

    Semi 저장 (게임프로젝트)
    오프닝 메서드//오프닝 클래스

---
+++ Opning.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Opning {
+    void Opening() {
+        System.out.println("------------------------------------");
+        System.out.println("------------------------------------");
+        System.out.println("어서오세요");
+        System.out.println("우리 마을에 오신 것을 환영합니다!");
+    }
+}
