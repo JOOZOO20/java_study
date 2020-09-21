7c8208f64328bab624f8c33ebb4479526d9f9e64
JOOZOO20 <qwsa7896@naver.com>
2020-09-21 22:52:40 +0900

    Semi 저장 (게임 프로젝트)
    메인 클래스

---
+++ Main.java
@@ -0,0 +1,35 @@
+package com.company;  // 얘 꼭 있어야하
+
+public class Main {
+    public static void main(String[] args) {
+        Opning a = new Opning();
+        void Open = a.Opening();
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//---------------memo line------------------
+//09.16
+// 지금 내용 빈 클래스 -> Building, CurePlace,FightPlace,Place,TrainingPlace
+// 생성자 사용하는걸로 바꾸기 super
+//Character class's method will changed to right things
+// 초반 도입부도 함수로 해서 걍 호출만 하기
+//상속의 종류 (extends, implements)
+//선언부 like basic frame form && 구현부 have method and instance
+
+
+//09.14
+//첫글자 대문자로
+//PageItem 띄어쓰기 들어가는 곳마다 대문자
+//클래스별로 파일 따로 (O)
