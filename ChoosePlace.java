7cb20e43252433035fa4528c1e4c1fd8ad3b9e1b
JOOZOO20 <qwsa7896@naver.com>
2020-09-22 23:32:14 +0900

    Semi 저장 (게임 프로젝트)
    장소어디로갈지 정하는 클래스

---
+++ ChoosePlace.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class ChoosePlace {
+    int ChooseNum = 0;
+
+    void printing(){
+        System.out.println("어느 곳으로 이동하시겠습니까?");
+        System.out.println("1번 치유공간");
+        System.out.println("2번 싸움터");
+        System.out.println("3번 상점");
+        System.out.println("4번 튜토리얼");
+    }
+}
